97fc34a209a6fa62ad5545ecd65044f24ab8689c
patriciaSR <patricia.suarez.psr@gmail.com>
2019-08-08 09:48:34 +0200

    add finder btn listener and fetch function to request show data

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,10 +2,41 @@
 
 //add querySelectors
 const showInput = document.querySelector('.finder__input');
-const showButton = document.querySelector('.finder__btn');
+const finderButton = document.querySelector('.finder__btn');
 const resultList = document.querySelector('.show-result__list');
 const favsList = document.querySelector('.show-favourites__list');
 
 
+const ENDPOINT = 'http://api.tvmaze.com/search/shows?q=';
+const defaultImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
+//listeners
+finderButton.addEventListener('click', searchShow);
 
+//Functions
+function searchShow() {
+  sendRequest();
+}
+
+function sendRequest() {
+  const showName = showInput.nodeValue;
+  fetch(ENDPOINT + showName)
+    .then(response => response.json())
+    .then(data => {
+      let showImage = '';
+
+      for (const item of data) {
+        if (item.show.image) {
+          showImage = item.show.image.medium;
+        } else {
+          showImage = defaultImage;
+        }
+
+        const showName = item.show.name;
+        const showId = item.show.id;
+
+        return createNewShowElement(showImage, showName, showId);
+
+      }
+    });
+}
